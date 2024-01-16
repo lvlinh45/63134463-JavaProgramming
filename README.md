939f39a2be48302ed44c51c2e00b0076cf36f810
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-01-16 20:55:12 +0700

    First Commit
    Commit đầu tiên

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 63134463-JavaProgramming
+Lưu các bài thực hành, dự án trong Lập trình Java
---
+++ getHello/src/getHello/hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package getHello;
+
+/**
+ * 
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+	}
+
+}
