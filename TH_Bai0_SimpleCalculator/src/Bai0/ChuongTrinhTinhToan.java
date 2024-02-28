8fac3376a5b9f539b13542cdd4e0db74cdeabf4e
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-02-28 15:35:05 +0700

    DONE TH_BAI0_SIMPLECALCULATOR

---
+++ TH_Bai0_SimpleCalculator/src/Bai0/ChuongTrinhTinhToan.java
@@ -0,0 +1,10 @@
+package Bai0;
+
+public class ChuongTrinhTinhToan {
+
+	public static void main(String[] args) {
+		// Tạo một màn hình GUI để tính toán
+		new ManHinhTinhToan().setVisible(true);
+	}
+
+}
