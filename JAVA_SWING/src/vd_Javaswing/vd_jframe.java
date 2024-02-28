a5d68132ba86ce65aadd865beba7ccda0fb70d66
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-02-28 14:08:42 +0700

    Update vd javaswing

--- JAVA_SWING/src/vd_Javaswing/vd_jframe.java
+++ JAVA_SWING/src/vd_Javaswing/vd_jframe.java
@@ -2,16 +2,29 @@ package vd_Javaswing;
 
 import java.awt.Window;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 public class vd_jframe {
 	public static void main(String[] args) {
-		JFrame f = new JFrame();
-		f.setTitle("Ví dụ về frame");
+		JFrame f = new JFrame(); // Tạo một thể hiên (đối tượng của lớp)
+		f.setTitle("Ví dụ về frame"); // tiêu đề cửa sổ
 		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		f.setSize(300,200);
-		f.show();
+		f.setSize(300,200); // kích thước cửa sổ
+		
+		
+		
+		// Tạo một nút bấm
+		JButton btnOK = new JButton("Nhấn tôi đi");
+		JButton btnKhac = new JButton("Nhấn em đi");
+		
+		// Gắn lên form
+		f.add(btnOK);
+		f.add(btnKhac);
+		// Chạy thử thì em đã ĐÈ lên tôi
+		
+		f.show(); // hiện cửa sổ
 		
 	}
 }
